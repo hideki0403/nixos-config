7bb21588ea0bd96cda8224d0699f5d8edf751c13
yukineko <27853966+hideki0403@users.noreply.github.com>
2026-07-24 16:14:23 +0900

    chore: update `hosts/ci`

--- hosts/ci/configuration.nix
+++ hosts/ci/configuration.nix
@@ -1,17 +1,10 @@
 { ... }:
 {
   imports = [
+    ./hardware-configuration.nix
     ../../modules/system
   ];
 
   networking.hostName = "ci";
   system.stateVersion = "25.11";
-
-  # for CI testing
-  boot.loader.systemd-boot.enable = true;
-
-  fileSystems."/" = {
-    device = "/dev/null";
-    fsType = "ext4";
-  };
 }
