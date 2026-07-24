53b9cd4819faa8a95dc45bf1553b55c8574c9e39
yukineko <27853966+hideki0403@users.noreply.github.com>
2026-07-24 15:26:40 +0900

    chore: バイナリキャッシュの提供元を追加

--- flake.nix
+++ flake.nix
@@ -29,6 +29,17 @@
     };
   };
 
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://noctalia.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+    ];
+  };
+
   outputs =
     {
       self,
