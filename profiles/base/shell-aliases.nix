56aec3698dd52c497c5f19c8dfc51357cf968e97
yukineko <27853966+hideki0403@users.noreply.github.com>
2026-08-10 13:12:13 +0900

    fix: ビルド時にsubmodulesが対象に含まれていなかった問題を修正

--- profiles/base/shell-aliases.nix
+++ profiles/base/shell-aliases.nix
@@ -1,8 +1,8 @@
 { ... }: {
   environment.shellAliases = {
-    nrs = "sudo nixos-rebuild switch --flake $HOME/nixos-config#$(hostname)";
-    nos = "nh os switch --ask $HOME/nixos-config -H $(hostname) --diff always";
-    nos-host = "nh os switch --ask $HOME/nixos-config -H ";
+    nrs = "sudo nixos-rebuild switch --flake \"git+file://$HOME/nixos-config?submodules=1#$(hostname)\"";
+    nos = "nh os switch --ask \"git+file://$HOME/nixos-config?submodules=1\" -H \"$(hostname)\" --diff always";
+    nos-host = "nh os switch --ask \"git+file://$HOME/nixos-config?submodules=1\" -H ";
     nca = "nh clean all --ask --keep-since 7d --keep 3";
   };
 }
