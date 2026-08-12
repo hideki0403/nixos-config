c586227d58a336b935167ed92df617dec4cfe79e
yukineko <27853966+hideki0403@users.noreply.github.com>
2026-08-12 13:10:00 +0900

    chore: cli.shで使用するパッケージのバージョンを固定

--- cli.sh
+++ cli.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p bash deno whois
+#!nix-shell -I nixpkgs=https://github.com/NixOS/nixpkgs/archive/fcb8fcd6bf2d0adecae5bd491afaaaf8311b758d.tar.gz
+#@nixpkgs: nixos-26.05
 
 set -euo pipefail
 
