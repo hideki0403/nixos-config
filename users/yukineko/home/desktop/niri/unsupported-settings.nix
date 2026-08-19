5beded6b93eb54e1b3e07f7748af7150effec44b
yukineko <27853966+hideki0403@users.noreply.github.com>
2026-08-19 11:49:31 +0900

    chore: niriのunsupported-settingsを`config.kdl`に切り出し

--- users/yukineko/home/desktop/niri/unsupported-settings.nix
+++ users/yukineko/home/desktop/niri/unsupported-settings.nix
@@ -3,10 +3,11 @@
   lib,
   pkgs,
   inputs,
+  mkSymlink,
   ...
 }:
 lib.mkIf (config.programs.niri.finalConfig != null) {
-  # Niriがまだサポートしていない設定を直接書き込む
+  # Niriがまだサポートしていない設定はconfig.kdlに書く
   # https://github.com/sodiboo/niri-flake/issues/1721#issuecomment-4428164218
   xdg.configFile.niri-config.source =
     let
@@ -19,102 +20,11 @@ lib.mkIf (config.programs.niri.finalConfig != null) {
 
         include optional=true "noctalia.kdl"
         // include optional=true "dms/colors.kdl"
-
-        // --- General settings ---
-        window-rule {
-          geometry-corner-radius 16
-
-          clip-to-geometry true
-
-          background-effect {
-            blur true
-            xray false
-          }
-
-          focus-ring {
-            width 2
-          }
-        }
-
-        layer-rule {
-          background-effect {
-            xray false
-          }
-        }
-
-        blur {
-          passes 2        // more passes = stronger blur (default: 3)
-          offset 3.0      // sample distance per pass (default: 3.0)
-          noise 0.03      // grain overlay (default: 0.02)
-          saturation 1.0  // color saturation boost (default: 1.5)
-        }
-
-        recent-windows {
-          binds {
-              Alt+Tab         { next-window; }
-              Alt+Shift+Tab   { previous-window; }
-          }
-        }
-
-        // --- Noctalia ---
-        layer-rule {
-          match namespace="^noctalia-(bar-[^\"]+|notification|dock|panel|attached-panel|osd)$"
-          background-effect {
-            xray false
-            blur true
-          }
-        }
-
-        layer-rule {
-          match namespace="^noctalia-backdrop"
-          place-within-backdrop true
-        }
-
-        window-rule {
-          match app-id="dev.noctalia.Noctalia"
-          open-floating true
-          default-column-width { fixed 1080; }
-          default-window-height { fixed 920; }
-        }
-
-        // --- DMS ---
-        // ref: https://danklinux.com/docs/dankmaterialshell/compositors#niri-configuration
-        layer-rule {
-            match namespace="^dms:clipboard$"
-            block-out-from "screencast"
-        }
-
-        layer-rule {
-            match namespace="^quickshell$"
-            place-within-backdrop true
-        }
-
-        window-rule {
-            match app-id=r#"^org\.gnome\."#
-            draw-border-with-background false
-        }
-
-        window-rule {
-            match app-id=r#"^org\.wezfurlong\.wezterm$"#
-            match app-id="Alacritty"
-            match app-id="zen"
-            match app-id="com.mitchellh.ghostty"
-            match app-id="kitty"
-            draw-border-with-background false
-        }
-
-        window-rule {
-            match is-active=false
-            focus-ring {
-                width 0
-            }
-        }
-
-        // Open DMS windows as floating by default
-        window-rule {
-            match app-id=r#"org.quickshell$"#
-            open-floating true
-        }
+        include optional=true "local.kdl"
       ''
     );
+
+  xdg.configFile."niri/local.kdl" = {
+    source = mkSymlink ./config.kdl;
+  };
 }
